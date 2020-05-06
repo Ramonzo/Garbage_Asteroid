95a7c86a696030d615e75192359c4e229dcbff30
Ramon Lago Alves Freire <ramonlago@outlook.com>
2020-05-06 00:17:40 -0300

    add resize window function

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+//Function to load assets and make loader works okay
 function loadAsset(){
   for(i = 0; i < imageNames.length; i++){
     loadedImages.push(loadImage(imgFolder+imageNames[i]+imgFormat, img => {assetsCounter++;}));
@@ -5,4 +6,8 @@ function loadAsset(){
   for(i = 0; i < fontNames.length; i++){
     loadedFonts.push(loadFont(fontFolder+fontNames[i]+fontFormat, font => {assetsCounter++;}));
   }
+}
+//Function to resize canvas screen
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
